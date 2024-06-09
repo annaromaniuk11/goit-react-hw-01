13ff3586dd183ba49984990abb39348e604dd2f4
annaromaniuk11 <143599695+annaromaniuk11@users.noreply.github.com>
2024-06-09 22:09:56 +0300

    hw

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import './App.module.css'
 import Profile from "../Profile/Profile.jsx"
 import userData from "../../userData.json"
-import FriendList from "../FriendLIst/FriendList.jsx"
+import FriendList from "../FriendList/FriendList.jsx"
 import friends from "../../friends.json"
 import TransactionHistory from "../TransactionHistory/TransactionHistory.jsx"
 import transactions from "../../transactions.json"
